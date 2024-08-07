69fb08838263532da761c66b0aa4fe3fa5b552f7
mapreader4 <mapreader4@gmail.com>
2024-08-07 17:51:50 -0400

    collect claude haiku responses

--- gen_claude_haiku.py
+++ gen_claude_haiku.py
@@ -5,9 +5,8 @@ import anthropic
 from datasets import load_dataset
 from tqdm import tqdm
 
-#TODO: fix rate limits
-rpm = 5
-rate_limit_delay = (60 / rpm) * 1.1
+rpm = 4000
+rate_limit_delay = 60 / rpm
 
 api_key = os.environ.get('ANTHROPIC_API_KEY')
 
@@ -18,16 +17,17 @@ client = anthropic.Anthropic(api_key="your-api-key-here")
 
 def process_question(question, prompt):
     messages = [
-                 {"role": "system", "content": prompt},
                  {"role": "user", "content": question}
                ]
     response = anthropic.Anthropic().messages.create(
         model="claude-3-haiku-20240307",
-        prompt=messages,
+        messages=messages,
+        max_tokens=4096,
+        system=prompt,
         temperature=0.6,
         top_p=0.9,
     )
-    return response.content
+    return response.content[0].text
 
 prompt = "You are solving a mathematics problem. While solving the problem, you think step by step, stating your " \
          "reasoning before stating any conclusions. To ensure your answer can be process, please conclude your " \
@@ -39,16 +39,21 @@ train_data = gsm8k["train"]
 output_dir = "claude_responses"
 os.makedirs(output_dir, exist_ok=True)
 
+next_question = 6362
+
 try:
-    for i, item in enumerate(tqdm(train_data)):
-        output = process_question(item["question"], prompt)
+    for i, item in enumerate(tqdm(train_data[next_question:]["question"]), start=next_question):
+        start_time = time.time()
+        output = process_question(item, prompt)
         
         file_name = f"response_{i:05d}.txt"
         file_path = os.path.join(output_dir, file_name)
         with open(file_path, 'w', encoding='utf-8') as f:
             f.write(output)
+        end_time = time.time()
         
-        time.sleep(rate_limit_delay)
+        if end_time - start_time < rate_limit_delay:
+            time.sleep(rate_limit_delay - (end_time - start_time))
 
 except Exception as e:
     print(f"An error occurred: {e}")
